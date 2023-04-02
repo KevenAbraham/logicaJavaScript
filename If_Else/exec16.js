dafaaf4d0e93d8a5433d2d935f1685de3c6ed2d5
unknown <abraham.keven20@gmail.com>
2023-04-02 15:59:14 -0300

    melhor exercicio

---
+++ If_Else/exec16.js
@@ -0,0 +1,7 @@
+let num = parseInt(prompt("Informe um valor:"))
+
+if (num % 2 == 0) {
+    document.write("<center>O número: " + num + " é <b>PAR");
+} else {
+    document.write("<center>O número: " + num + " é <b>ÍMPAR");
+}
